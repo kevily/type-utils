1ffba8788d0320dad883b42a1038cd721424f6ca
1k <bug_zero@163.com>
2023-11-20 10:44:04 +0800

    fix: build

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,7 @@
 import { Engine, tsc } from '1k-tasks'
 import * as path from 'path'
 
-const reactTask = new Engine({ root: path.join(process.cwd(), 'packages/type-tools') })
+const reactTask = new Engine({ root: path.join(process.cwd(), 'packages/types') })
 reactTask.registry('tsc', tsc)
 
 reactTask.run()
